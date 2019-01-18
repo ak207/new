be841476b73e0496aabe90209bcff8e8c8bb1740
OnlDisRes <46018035+ak207@users.noreply.github.com>
2019-01-18 14:38:12 +0530

    initial commit

---
+++ HelloWorld/src/Calculator.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+
+/**
+ * @author ak207
+ *
+ */
+public class Calculator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		long a=100, b=100;
+		System.out.println("The inputs are " + a + " " + b);
+		System.out.println("Adding values "+ (a+b));
+		System.out.println("Subtract values "+ (a-b));
+		System.out.println("Multiply values "+ (a*b));
+		System.out.println("Divide values "+ (a/b));
+		
+		
+	}
+
+}
